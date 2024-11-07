bacc9a691a5fa6d7c3e0c201af056f7e814770d6
emazupp <zuppardo.emanuele2001@gmail.com>
2024-11-07 17:31:14 +0100

    feat: add modal

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-const cardsContainerEL = document.getElementById("cards-container");
+const cardsContainerEl = document.getElementById("cards-container");
 const cardsLimit = 6;
+const modalContainerEl = document.getElementById("image-modal-container");
+const modalEl = document.getElementById("image-modal");
+const closeModalButton = document.getElementById("close-modal-button");
+
+const closeModal = () => {
+  modalContainerEl.classList.add("d-none");
+};
 
 fetch("https://jsonplaceholder.typicode.com/photos?_limit=" + cardsLimit)
   .then((res) => res.json())
@@ -15,12 +22,14 @@ fetch("https://jsonplaceholder.typicode.com/photos?_limit=" + cardsLimit)
             </div>
           </div> `;
     }
-    cardsContainerEL.innerHTML = cardsContentHtml;
+    cardsContainerEl.innerHTML = cardsContentHtml;
     const cardsEl = document.querySelectorAll(".card");
 
     for (card of cardsEl) {
       card.addEventListener("click", function () {
-        console.log(this.children[0].getAttribute("src"));
+        modalEl.src = this.children[0].getAttribute("src");
+        modalContainerEl.classList.remove("d-none");
       });
+      closeModalButton.addEventListener("click", closeModal);
     }
   });
