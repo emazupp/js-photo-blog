55c3109f91b9b5923a5bd903fd7bdb0e1508a3f9
emazupp <zuppardo.emanuele2001@gmail.com>
2024-11-08 12:39:03 +0100

    fixes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-const mainEl = document.getElementById("main");
-const headerEl = document.getElementById("header");
 const cardsContainerEl = document.getElementById("cards-container");
 const cardsLimit = 6;
 const modalContainerEl = document.getElementById("image-modal-container");
@@ -8,8 +6,12 @@ const closeModalButton = document.getElementById("close-modal-button");
 
 const closeModal = () => {
   modalContainerEl.classList.add("d-none");
-  mainEl.classList.remove("obfuscated");
-  headerEl.classList.remove("obfuscated");
+};
+
+const upperCaseFirstLetter = (title) => {
+  const firstLetterUpperCased = title[0].toUpperCase();
+  const titleExceptFirstLetter = title.substring(1);
+  return firstLetterUpperCased + titleExceptFirstLetter;
 };
 
 fetch("https://jsonplaceholder.typicode.com/photos?_limit=" + cardsLimit)
@@ -17,11 +19,12 @@ fetch("https://jsonplaceholder.typicode.com/photos?_limit=" + cardsLimit)
   .then((photo) => {
     let cardsContentHtml = "";
     for (el of photo) {
+      const upperCasedTitle = upperCaseFirstLetter(el.title);
       cardsContentHtml += `<div class="col-md-6 col-lg-4">
             <div class="card border-1">
               <img class="p-3" src="${el.url}" alt="img" />
               <div class="card-body">
-                <p class="card-text fs-5">${el.title}</p>
+                <p class="card-text fs-5">${upperCasedTitle}</p>
               </div>
             </div>
           </div> `;
@@ -33,9 +36,10 @@ fetch("https://jsonplaceholder.typicode.com/photos?_limit=" + cardsLimit)
       card.addEventListener("click", function () {
         modalEl.src = this.children[0].getAttribute("src");
         modalContainerEl.classList.remove("d-none");
-        mainEl.classList.add("obfuscated");
-        headerEl.classList.add("obfuscated");
+        modalContainerEl.classList.add("obfuscated");
       });
       closeModalButton.addEventListener("click", closeModal);
     }
   });
+
+/* Picsum: https://picsum.photos/600 */
