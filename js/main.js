7681542413d8dcb407ed49ae0c88eeb18ea2eda0
emazupp <zuppardo.emanuele2001@gmail.com>
2024-11-12 17:39:20 +0100

    saves

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ fetch("https://jsonplaceholder.typicode.com/photos?_limit=" + cardsLimit)
             <div class="card border-1">
               <img class="p-3" src="${el.url}" alt="img" />
               <div class="card-body">
-                <p class="card-text fs-5">${upperCasedTitle}</p>
+                <p class="card-text">${upperCasedTitle}</p>
               </div>
             </div>
           </div> `;
